99e403663484b11144d9e15f2806d165bd77ec31
Marcelo Saviski <marcelosaviski@gmail.com>
2020-06-15 22:26:42 -0300

    fixed minification

--- rollup.config.js
+++ rollup.config.js
@@ -2,25 +2,29 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from "rollup-plugin-terser";
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [{
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
       file: pkg.main
-    }, { 
-      file: pkg.module,
-      pugins: [
-        terser({
-          output: {
-            comments: false
-          }
-        })
-      ]
-    }
-  ],
-  external: Array.from(Object.keys(pkg.dependencies || {})),
-  plugins: [
-    typescript({
-      useTsconfigDeclarationDir: true
-    })
-  ]
-}
+    },
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true
+      })
+    ]
+  },
+  {
+    input: pkg.main,
+    output: { 
+      file: pkg.module
+    },
+    plugins: [
+      terser({
+        output: {
+          comments: false
+        }
+      })
+    ]
+  }
+]
