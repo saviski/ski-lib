b7920fa934d482a3be64d18c70380351bdb04548
Marcelo Saviski <marcelosaviski@gmail.com>
2020-06-16 19:06:29 -0300

    including js in package build

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,20 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from "rollup-plugin-terser";
 import pkg from './package.json';
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      file: pkg.main
-    },
-    plugins: [
-      typescript({
-        useTsconfigDeclarationDir: true
-      })
-    ]
+export default {
+  input: 'src/index.ts',
+  output: { 
+    file: pkg.main,
+    sourcemap: true
   },
-  {
-    input: pkg.main,
-    output: { 
-      file: pkg.module
-    },
-    plugins: [
-      terser({
-        output: {
-          comments: false
-        }
-      })
-    ]
-  }
-]
+  plugins: [
+    typescript({
+      useTsconfigDeclarationDir: true
+    }),
+    terser({
+      output: {
+        comments: false
+      }
+    })
+  ]
+}
